913ec275e83385c6ad78cb63cbc9a144ca095ff4
la .uetcis. <uetcis@pm.me>
2025-01-10 02:23:17 -0800

    Update

--- niri/.config/niri/Xwayland.sh
+++ niri/.config/niri/Xwayland.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
 env Xwayland :0 -hidpi -force-xrandr-emulation &
-sleep 0.1
+sleep 1
 env -u WAYLAND_DISPLAY -u XDG_SESSION_TYPE GTK_IM_MODULE=fcitx WINIT_X11_SCALE_FACTOR=2 GDK_SCALE=2 DISPLAY=:0 xmonad
