fe01c494afe367a425c64be8912678084c88b810
la .uetcis. <uetcis@pm.me>
2025-01-28 01:35:48 -0800

    Update

--- niri/.config/niri/drun.fish
+++ niri/.config/niri/drun.fish
@@ -5,5 +5,5 @@ set id (niri msg -j focused-window | jq ".app_id")
 if echo $id | grep 'org.freedesktop.Xwayland' > /dev/null
   killall rofi || env -u WAYLAND_DISPLAY -u XDG_SESSION_TYPE GTK_IM_MODULE=fcitx DISPLAY=:0 rofi -show drun -dpi 163
 else
-  killall fuzzel || fuzzel
+  killall fuzzel || fuzzel -T "alacritty -e"
 end
